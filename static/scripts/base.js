5719047af683e81cc80d26b7096814af052612bd
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-02-13 14:15:20 +0530

    profile

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,7 +1,15 @@
-const menu=document.getElementsByTagName('a');
+function open_log(){
+    let pro=document.getElementById('pro');
+    console.log("clicked");
 
-// menu[0].addEventListener('mouseover',()=>
-// {
-//     console.log('hey');
-//     menu[0].style.backgroundColor='black';
-// });
+    if(pro.style.display==="flex")
+    {
+        console.log('inside if')
+        pro.style.display="none";
+    }
+    else 
+    {
+        console.log('inside else')
+        pro.style.display="flex";
+    }
+}
